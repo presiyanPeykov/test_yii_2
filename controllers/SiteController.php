6caa818ea822b176351c00e087e53d98078abaa1
presiyanp <presiyan@celtis.net>
2015-01-09 11:42:29 +0200

    adding entry form and model

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -8,6 +8,7 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\EntryForm;
 
 class SiteController extends Controller
 {
@@ -95,7 +96,27 @@ class SiteController extends Controller
     }
 
 
+	public function actionSay($message = 'Hello'){
+
+		return $this->render('say', ['message'=>$message]);
+	}
 
 
-	pub
+	public function actionEntry()
+	{
+		$model = new EntryForm;
+
+		if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+			// valid data received in $model
+
+			// do something meaningful here about $model ...
+
+			return $this->render('entry-confirm', ['model' => $model]);
+		} else {
+			// either the page is initially displayed or there is some validation error
+			return $this->render('entry', ['model' => $model]);
+		}
+	}
+
+
 }
